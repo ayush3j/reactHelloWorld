07485b38320c7c0f873107d6b949698228d25f1b
Ayush Chaudhary <ayush3j@gmail.com>
2022-06-23 13:09:49 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
+import Button from "./Button";
+function App()
+{
+    return(
+        <div>
+            <h1 style={{textAlign: "center"}}>Hello World!</h1>   
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Button title="App Store"/>
+            <Button title=" Play Store"/>
+            
+            {/* <button class= "button"><h1>Play Store</h1></button>
+            <button class= "button"><h1>App Store</h1></button> */}
+        </div>
+    )  
 }
-
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Button = ({title}) => (
+    <div>
+        <button class= "button"><h1>{title}</h1></button>
+    </div>
+)
+export default Button;
